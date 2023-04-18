25f3846d950490c5dcd161456b69c10adfaccd7e
shaikat17 <shaikatpal56@gmail.com>
2023-04-18 15:07:46 +0600

    added background image

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import StartGame from './screens/StartGame';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground source={require('./assets/images/background.png')} resizeMode='cover' style={styles.rootScreen} imageStyle={styles.backgroundImage} >
       <StartGame/>
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -14,5 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
      flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
