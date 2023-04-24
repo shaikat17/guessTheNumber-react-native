5236241e6636343e160956ffeb475f5a2fba9d3c
shaikat17 <shaikatpal56@gmail.com>
2023-04-25 00:03:13 +0600

    working on game over screen and an image added to the game over screeen

--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Title from '../components/UI/Title';
+import Colors from '../components/constants/Colors';
 
 const GameOver = () => {
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.rootContainer}>
+            <Title text='Game Over'></Title>
+            <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require('../assets/images/success.png')} />
+            </View>
+            <View>
+                <Text>Your phone needed X rounds to guess the number Y.</Text>
+            </View>
         </View>
     );
 };
 
 export default GameOver;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        borderRadius: 150,
+        width: 300,
+        height: 300,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+})
